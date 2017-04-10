aca3447dc12d5601840d39b39a0ee7fc04d14e71
HeyWeekly <steaest@qq.com>
2017-04-10 21:32:03 +0800

    项目初始化

---
+++ Mr.Time/Mr.Time/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mr.Time
+//
+//  Created by 王伟伟 on 2017/4/10.
+//  Copyright © 2017年 Offape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mr.Time/Mr.Time/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mr.Time
+//
+//  Created by 王伟伟 on 2017/4/10.
+//  Copyright © 2017年 Offape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
